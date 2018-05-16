e5f9689d2d007b7efccc3984aa547cb099f32465
Sadohin Vitaliy <vitaliysadohin@gmail.com>
2018-05-16 08:51:18 +0300

    Сode restructure: extraction of auxiliary methods

--- addressbook-web-tests/GruopCreationTests.cs
+++ addressbook-web-tests/GruopCreationTests.cs
@@ -47,10 +47,10 @@ namespace WebAddressbookTests
         public void GroupCreationTest()
         {
             OpenHomePage();
-            Login();
+            Login("admin", "secret");
             GoToGroupPage();
             InitNewGroupCreation();
-            FillGroupForm();
+            FillGroupForm("aaa", "sss", "ddd");
             SubmitGroupCreation();
             ReturnToGroupPage();
         }
@@ -68,15 +68,15 @@ namespace WebAddressbookTests
             driver.FindElement(By.Name("submit")).Click();
         }
 
-        private void FillGroupForm()
+        private void FillGroupForm(string name, string header, string footer)
         {
             
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("fg");
+            driver.FindElement(By.Name("group_name")).SendKeys(name);
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("fg");
+            driver.FindElement(By.Name("group_header")).SendKeys(header);
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("gf");
+            driver.FindElement(By.Name("group_footer")).SendKeys(footer);
         }
 
         private void InitNewGroupCreation()
@@ -91,13 +91,13 @@ namespace WebAddressbookTests
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
-        private void Login()
+        private void Login(string username, string password)
         {
             
             driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
+            driver.FindElement(By.Name("user")).SendKeys(username);
             driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
+            driver.FindElement(By.Name("pass")).SendKeys(password);
             driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
         }
 
